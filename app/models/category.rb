e7a3f1cf6da4bcb42be653921b8623e34c2cdade
jojojoker893 <jojojoker114514@gmail.com>
2025-02-02 20:23:58 +0900

    モデル名clothをclothingへ変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :cloth
+  has_many :clothing
 end
--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -1,4 +1,4 @@
-class Cloth < ApplicationRecord
+class Clothing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   belongs_to :brand
