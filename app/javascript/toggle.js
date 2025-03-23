c4fd47eea9063af045e8730a6e0027977374363b
jojojoker893 <jojojoker114514@gmail.com>
2025-03-23 10:13:35 +0900

    add: filter用のレイアウト、image追加

--- app/javascript/toggle.js
+++ app/javascript/toggle.js
@@ -1,11 +1,16 @@
 document.addEventListener("turbo:load", () => {
-  const icon = document.getElementById("search-toggle")
-  const form = document.getElementById("search-form")
+  toggleVisialize("search-toggle", "search-form");
+  toggleVisialize("filter-toggle", "filter-form");
 
-  if (icon && form) {
-    icon.addEventListener("click",() => {
-      form.classList.toggle("toggle-vision")
-      form.classList.toggle("toggle-hidden")
-    });
+  function toggleVisialize(toggleId, formId) {
+    const toggle = document.getElementById(toggleId);
+    const form = document.getElementById(formId);
+
+    if(toggle && form) {
+      toggle.addEventListener("click", () => {
+        form.classList.toggle("toggle-vision");
+        form.classList.toggle("toggle-hidden");
+      });
+    }
   }
 });
