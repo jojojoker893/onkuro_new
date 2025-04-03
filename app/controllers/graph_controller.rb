ba0ccf0fe5530a61f457a470aa16c292fc560140
jojojoker893 <jojojoker114514@gmail.com>
2025-04-03 21:33:59 +0900

    fix: 指定した期間でグラフにデータが取得できるようにした

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -1,9 +1,17 @@
 class GraphController < ApplicationController
   def index
-    @use_logs = current_user.clothing
-      .joins(:clothing_usage_logs)
-      .group("clothings.id")
-      .pluck("clothings.name, COUNT(clothing_usage_logs.id)")
-      .map { |name, count| { name: name, y: count } }
+    startdate = Date.parse(params[:start_date]) rescue nil
+    enddate = Date.parse(params[:end_date]) rescue nil
+
+    if startdate.present? && enddate.present?
+      usage_data = ClothingUsageLog.usage_period(startdate, enddate)
+    else
+      usage_data = current_user.clothing
+        .joins(:clothing_usage_logs)
+        .group("clothings.id")
+        .pluck("clothings.name, COUNT(clothing_usage_logs.id)")
+    end
+
+    @graph_data = usage_data.map { |name, count| { name: name, y: count } }
   end
 end
--- app/models/clothing_usage_log.rb
+++ app/models/clothing_usage_log.rb
@@ -3,4 +3,10 @@ class ClothingUsageLog < ApplicationRecord
   belongs_to :user
 
   validates :used_at, presence: true
+
+  def self.usage_period(startdate, enddate)
+    where(created_at: startdate.beginning_of_day..enddate.end_of_day)
+    .group(:clothing_id)
+    .count
+  end
 end
