d01a3858088ade3b14cfa14df21f7ec70de2ef08
jojojoker893 <jojojoker114514@gmail.com>
2025-04-28 20:58:26 +0900

    fix: モデルに記述したロジックをuse_caseクラスにした

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -3,7 +3,7 @@ class ClothingsController < ApplicationController
     @categories = Category.all
     @brands = Brand.all
     @colors = Color.all
-    @clothings = Clothing.search_with_params(user: current_user, params: params)
+    @clothings = ClothingSearchUseCase.new(user: current_user, params: params).call
   end
 
   def new
---
+++ app/use_cases/clothing_search_use_case.rb
@@ -0,0 +1,23 @@
+class ClothingSearchUseCase
+  def initialize(user:, params:)
+    @user = user
+    @params = params
+  end
+
+  def call
+    clothings = @user.clothings
+    clothings = clothings.search_keyword(@params[:keyword]) if @params[:keyword].present?  # 検索
+    clothings = clothings.filter_category(@params[:category_id]) if @params[:category_id].present? # カテゴリフィルター
+    clothings = clothings.filter_brand(@params[:brand_id]) if @params[:brand_id].present? # ブランドフィルター
+    clothings = clothings.filter_color(@params[:color_id]) if @params[:color_id].present? # カラーフィルター
+
+    case @params[:order] # ソート機能
+    when "usage_asc", "usage_desc"
+      clothings = clothings.usage_log_count.order_usage(@params[:order] == "usage_asc" ? "ASC" : "DESC")
+    else
+      clothings = clothings.order_created_at
+    end
+
+    clothings
+  end
+end
