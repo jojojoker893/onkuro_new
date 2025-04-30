d6284585fc5cd755540eb6bfa2fad04bb8ffaf36
jojojoker893 <jojojoker114514@gmail.com>
2025-04-30 13:39:56 +0900

    fix: graph_controllerをuse_cs

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -3,15 +3,10 @@ class GraphController < ApplicationController
     startdate = Time.zone.parse(params[:startdate]) rescue nil
     enddate = Time.zone.parse(params[:enddate]) rescue nil
 
-    if startdate.present? && enddate.present?
-      usage_data = ClothingUsageLog.usage_period(current_user, startdate, enddate)
-      @graph_data = usage_data.map { |name, count| { name: name, y: count } }
-    else
-      usage_data = current_user.clothings
-      .joins(:clothing_usage_logs)
-      .group("clothings.id")
-      .pluck("clothings.name, COUNT(clothing_usage_logs.id)")
-      @graph_data = usage_data.map { |name, count| { name: name, y: count } }
-    end
+    @graph_data = GraphUseCase.new(
+      user: current_user,
+      startdate: startdate,
+      enddate: enddate
+    ).call
   end
 end
