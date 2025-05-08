8e03a4afacdca702502b4098793d251592b7c28f
jojojoker893 <jojojoker114514@gmail.com>
2025-05-08 12:30:26 +0900

    fix: クラス名を動詞から名詞へ変更し、userではuser_idのみ渡すように修正

---
+++ app/use_cases/clothing_Report.rb
@@ -0,0 +1,25 @@
+class ClothingReport
+  attr_reader :user_id, :params
+
+  def initialize(user_id:, params:)
+    @user_id = user_id
+    @params = params
+  end
+
+  def call
+    clothings = Clothing.where(user_id: user_id)
+    clothings = clothings.search_keyword(params[:keyword]) if params[:keyword].present?  # 検索
+    clothings = clothings.filter_category(params[:category_id]) if params[:category_id].present? # カテゴリフィルター
+    clothings = clothings.filter_brand(params[:brand_id]) if params[:brand_id].present? # ブランドフィルター
+    clothings = clothings.filter_color(params[:color_id]) if params[:color_id].present? # カラーフィルター
+
+    case params[:order] # ソート機能
+    when "usage_asc", "usage_desc"
+      clothings = clothings.usage_log_count.order_usage(params[:order] == "usage_asc" ? "ASC" : "DESC")
+    else
+      clothings = clothings.order_created_at
+    end
+
+    clothings
+  end
+end
