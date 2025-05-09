b104e2c89dbc535a55357c912e3fa676306b5b3d
jojojoker893 <jojojoker114514@gmail.com>
2025-05-09 12:46:57 +0900

    delete: clothing_search.rbをclothing_report.rbへ変更した為削除

--- app/use_cases/update_password.rb
+++ app/use_cases/update_password.rb
@@ -6,6 +6,13 @@ class UpdatePassword
   end
 
   def call
+    pass = update_current_password
+  end
+
+  private
+  attr_reader :user, :current_password, :new_params
+
+  def update_current_password
     if @user.authenticate(@current_password)
       @user.update(@new_params)
     else
