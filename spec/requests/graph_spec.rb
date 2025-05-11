e5a95f6df7fb3f2b4cb629fc1ad414dfbcc8eff5
jojojoker893 <jojojoker114514@gmail.com>
2025-05-11 21:14:49 +0900

    fix: graph_specを修正

--- spec/requests/graph_spec.rb
+++ spec/requests/graph_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Graphs", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/graph/index"
+      get "/graph"
       expect(response).to have_http_status(:success)
     end
   end
