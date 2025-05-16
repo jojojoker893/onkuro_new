3ce9482a0ebfa4c5eaff7ccc8500a4a75b88c57d
jojojoker893 <jojojoker114514@gmail.com>
2025-05-16 12:56:19 +0900

    add: expectが複数あるitブロックにaggregate_failuresを追加

--- spec/use_cases/clothing_report_spec.rb
+++ spec/use_cases/clothing_report_spec.rb
@@ -5,39 +5,48 @@ RSpec.describe ClothingReport, type: :model do
   let!(:clothing1) { FactoryBot.create(:clothing, user: user, name: '白シャツ') }
   let!(:clothing2) { FactoryBot.create(:clothing, user: user, name: '黒シャツ') }
 
-  context "キーワードを入力した場合" do
-    it "絞り込めること" do
-      result = ClothingReport.new(user_id: user.id, params: { keyword: '白' }).call
-
-      expect(result).to include(clothing1)
-      expect(result).not_to include(clothing2)
-    end
-  end
-
   before do
     create_list(:clothing_usage_log, 2, user: user, clothing: clothing1)
     create(:clothing_usage_log, user: user, clothing: clothing2)
   end
 
-  context "usage_asc を指定した場合" do
+  context "キーワードを入力した場合" do
+    it "絞り込めること" do
+      aggregate_failures do
+        result = ClothingReport.new(user_id: user.id, params: { keyword: '白' }).call
+
+        expect(result).to include(clothing1)
+        expect(result).not_to include(clothing2)
+      end
+    end
+  end
+
+  context "usage_ascを指定した場合" do
     it "使用回数が少ない順に並ぶ" do
-      result = ClothingReport.new(user_id: user.id, params: { order: "usage_asc" }).call
-      expect(result.first).to eq(clothing2)
-      expect(result.second).to eq(clothing1)
+      aggregate_failures do
+        result = ClothingReport.new(user_id: user.id, params: { order: "usage_asc" }).call
+
+        expect(result.first).to eq(clothing2)
+        expect(result.second).to eq(clothing1)
+      end
     end
   end
 
-  context "usage_desc を指定した場合" do
+  context "usage_descを指定した場合" do
     it "使用回数が多い順に並ぶ" do
-      result = ClothingReport.new(user_id: user.id, params: { order: "usage_desc" }).call
-      expect(result.first).to eq(clothing1)
-      expect(result.second).to eq(clothing2)
+      aggregate_failures do
+        result = ClothingReport.new(user_id: user.id, params: { order: "usage_desc" }).call
+
+        expect(result.first).to eq(clothing1)
+        expect(result.second).to eq(clothing2)
+      end
     end
   end
 
-  context "order パラメータが空の場合" do
+  context "orderパラメータが空の場合" do
     it "created_at順になる" do
       result = ClothingReport.new(user_id: user.id, params: {}).call
+
       expect(result.first.created_at).to be > result.second.created_at
     end
   end
