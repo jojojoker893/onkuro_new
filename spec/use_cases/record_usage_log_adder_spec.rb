eed8feb3859b75cdd1c1082c06fa12c2557f9945
jojojoker893 <jojojoker114514@gmail.com>
2025-05-18 17:45:03 +0900

    fix: aggregate_failuresを追加

--- spec/use_cases/record_usage_log_adder_spec.rb
+++ spec/use_cases/record_usage_log_adder_spec.rb
@@ -7,12 +7,14 @@ let!(:clothing2) { FactoryBot.create(:clothing, user: user, name: '黒シャツ'
 
 context "使用回数を記録した時" do
   it "used_logが追加される" do
-    expect { RecordUsageLogAdder.new(user: user, clothing_id: clothing1.id).call }
-    .to change { ClothingUsageLog.count }.by(1)
+    aggregate_failures do
+      expect { RecordUsageLogAdder.new(user: user, clothing_id: clothing1.id).call }
+      .to change { ClothingUsageLog.count }.by(1)
 
-    log = ClothingUsageLog.last
-    expect(log.user).to eq(user)
-    expect(log.clothing).to eq(clothing1)
+      log = ClothingUsageLog.last
+      expect(log.user).to eq(user)
+      expect(log.clothing).to eq(clothing1)
+      end
     end
   end
 end
