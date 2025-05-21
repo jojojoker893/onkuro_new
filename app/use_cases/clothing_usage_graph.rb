ee06397d6ac4116d69bbbcdc256ced08c9145736
jojojoker893 <jojojoker114514@gmail.com>
2025-05-21 12:26:12 +0900

    fix: コメント追記

--- app/use_cases/clothing_usage_graph.rb
+++ app/use_cases/clothing_usage_graph.rb
@@ -16,8 +16,10 @@ class ClothingUsageGraph
 
   def usage_data_range_or_all
     if start_date.present? && end_date.present?
+      # 両方の日付が指定されている場合は、その期間に絞って使用回数を取得
       ClothingUsageLog.usage_period(user.id, start_date, end_date)
     else
+      # 日付が未指定なら、全ての服のログ使用ログを集計(服のIDごとにグループ化)
       user.clothings
       .joins(:clothing_usage_logs)
       .group("clothings.id")
