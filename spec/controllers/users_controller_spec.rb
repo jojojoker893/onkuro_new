171546481baf361661377e6876247e827d5b39b0
jojojoker893 <jojojoker114514@gmail.com>
2025-05-29 15:30:15 +0900

    add: パスワード更新のテストを追加

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "#new" do
+  let(:user) { create(:user, password: "password") }
+
+  before do
+    session[:user_id] = user.id
+  end
+
+  describe "users_controller" do
+    context "現在のパスワードを入力した場合" do
+      it "パスワードが更新される" do
+        patch :password_update, params: {
+          user: {
+            current_password: "password",
+            password: "new_password",
+            password_confirmation: "new_password"
+          }
+        }
+
+        expect(response).to redirect_to edit_password_user_path
+        expect(flash[:notice]).to eq("パスワードを変更しました")
+      end
+    end
+  end
+
+  context "#new" do
     it "正常にレスポンスを返すこと" do
       get :new
       expect(response).to be_successful
