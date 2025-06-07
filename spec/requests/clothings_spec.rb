feb1fc3ba36df8306a35d321cad5d091c2f32c9c
jojojoker893 <jojojoker114514@gmail.com>
2025-06-07 20:17:27 +0900

    add: createまでのテストを記述

--- spec/requests/clothings_spec.rb
+++ spec/requests/clothings_spec.rb
@@ -7,16 +7,16 @@ RSpec.describe "Clothings", type: :request do
   let!(:color) { create(:color) }
   let!(:clothing) { create(:clothing, user: user, category: category, brand: brand, color: color) }
 
-  context "ログインしている場合" do
-    before do
-      post "/login", params: {
-        session: {
-          email: user.email,
-          password: "sample_password"
-        }
+  before do # ログイン状態の作成
+    post "/login", params: {
+      session: {
+        email: user.email,
+        password: user.password
       }
+    }
     end
 
+  context "一覧ページの表示" do
     it "一覧ページへのリクエストが200OKであること" do
       get clothings_path
       expect(response.status).to eq 200
@@ -25,6 +25,30 @@ RSpec.describe "Clothings", type: :request do
     it "自分の服のみ表示されること" do
       get clothings_path
       expect(response.body).to include(clothing.name)
+    end
+  end
+
+  context "新規作成ページ表示" do
+    it "新規作成ページへのリクエストが200OKであること" do
+      get new_clothing_path
+      expect(response.status).to eq 200
+    end
+  end
+
+  context "服の登録" do
+    it "正しいパラメーターで登録されること" do
+      expect {
+        post "/clothings", params: {
+          clothing: {
+            name: "new_clothing",
+            category_id: category.id,
+            brand_id: brand.id,
+            color_id: color.id
+          }
+        }
+      }.to change(Clothing, :count).by(1)
+      redirect_to clothings_path
+      expect(response.status).to eq 302
     end
   end
 end
