54edd6213f62e584fbf7d86811de78a5ebf46ef1
jojojoker893 <jojojoker114514@gmail.com>
2025-06-13 20:41:54 +0900

    fix: 命名を修正

--- app/models/clothing.rb
+++ app/models/clothing.rb
@@ -11,12 +11,12 @@ class Clothing < ApplicationRecord
   scope :order_by_created_at, -> { order(created_at: :desc) }
 
   # @@使用回数の降順、昇順で並び替え
-  scope :order_usage, ->(sort_order = "DESC") { order(Arel.sql("usage_count #{sort_order}")) }
+  scope :order_by_usage, ->(sort_order = "DESC") { order(Arel.sql("usage_count #{sort_order}")) }
 
   # @@絞り機能 カテゴリ、ブランド、カラー
-  scope :filter_category, ->(category_id) { where(category_id: category_id)  if category_id.present? }
-  scope :filter_brand, ->(brand_id) { where(brand_id: brand_id)  if brand_id.present? }
-  scope :filter_color, ->(color_id) { where(color_id: color_id)  if color_id.present? }
+  scope :filter_by_category, ->(category_id) { where(category_id: category_id)  if category_id.present? }
+  scope :filter_by_brand, ->(brand_id) { where(brand_id: brand_id)  if brand_id.present? }
+  scope :filter_by_color, ->(color_id) { where(color_id: color_id)  if color_id.present? }
 
   # @@検索機能
   scope :search_keyword, ->(keyword) { where("name LIKE ?", "%" + keyword + "%") }
