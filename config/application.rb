a649ee4b1f027271955eb00a84d74d8705863612
jojojoker893 <jojojoker114514@gmail.com>
2025-06-14 13:27:07 +0900

    fix: rubocopで自動修正

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module OnkuroNew
     config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
