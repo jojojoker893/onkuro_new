8c777d7d9fd1bfe4b48b905a280df15e74b60e6c
jojojoker893 <jojojoker114514@gmail.com>
2025-06-29 21:49:21 +0900

    style: 余分な空白やコメント、空行を消す

--- app/use_cases/clothing_report.rb
+++ app/use_cases/clothing_report.rb
@@ -2,7 +2,7 @@ class ClothingReport
   ORDER_DIRECTIONS = {
     "usage_asc" => "ASC",
     "usage_desc" => "DESC"
-}.freeze
+  }.freeze
 
   def initialize(user_id:, params:)
     @user_id = user_id
@@ -11,11 +11,10 @@ class ClothingReport
 
   def call
     clothings = Clothing.where(user_id: user_id)
-    clothings = clothings.search_keyword(params[:keyword]) if params[:keyword].present?  # 検索
-    clothings = clothings.filter_by_category(params[:category_id]) if params[:category_id].present? # カテゴリフィルター
-    clothings = clothings.filter_by_brand(params[:brand_id]) if params[:brand_id].present? # ブランドフィルター
-    clothings = clothings.filter_by_color(params[:color_id]) if params[:color_id].present? # カラーフィルター
-
+    clothings = clothings.search_keyword(params[:keyword]) if params[:keyword].present?
+    clothings = clothings.filter_by_category(params[:category_id]) if params[:category_id].present?
+    clothings = clothings.filter_by_brand(params[:brand_id]) if params[:brand_id].present?
+    clothings = clothings.filter_by_color(params[:color_id]) if params[:color_id].present?
 
     apply_ordering(clothings)
   end
@@ -24,17 +23,16 @@ class ClothingReport
 
   attr_reader :user_id, :params
 
-  def apply_ordering(clothings) # 並び替え
+  def apply_ordering(clothings)
     if order_direction.present?
-      # @@使用回数を取得
       result = clothings.joins(:clothing_usage_logs)
-      .group("clothings.id")
-      .order("COUNT(clothing_usage_logs.id) #{order_direction}")
-      .to_a
+                        .group("clothings.id")
+                        .order("COUNT(clothing_usage_logs.id) #{order_direction}")
+                        .to_a
 
       Kaminari.paginate_array(result)
-      .page(params[:page])
-      .per(8)
+              .page(params[:page])
+              .per(8)
     else
       clothings.order_by_created_at.page(params[:page]).per(8)
     end
