1d441a3a1f79714f3d905f79e93d85252676adeb
jojojoker893 <jojojoker114514@gmail.com>
2025-06-29 23:47:24 +0900

    feat: ActiveRecord::RecordNotFoundをrescue_fromして、404ページに遷移させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   helper_method :current_user
   # TODO: ActiveRecord::RecordNotFoundをrescue_fromして、404ページに遷移させる
-  rescue_from ActiveRecord::RecordNotFound, render 
-
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def record_not_found
+    render plain: "404 Not Found", status: 404
   end
 end
